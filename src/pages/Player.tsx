25fa6972a24f6c6254f1c8082f0d9bbd8ec44a07
lithproroot1399 <prorootinfo@gmail.com>
2024-01-22 18:50:32 +0000

    Adicionanco scrollbar

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -26,7 +26,7 @@ export function Player() {
                     />
                 </div>
                 </div>
-                <aside className='w-80 absolute top-8 bottom-8 right-8 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800'>
+                <aside className='w-80 absolute top-0 botton-0 right-0 border-l border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800'>
                     <div>
                         <button className='flex w-full items-center gap-3 bg-zinc-800 p-4'>
                             <div className='flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs'>
@@ -34,11 +34,11 @@ export function Player() {
                                 </div>
 
                                 <div className='flex flex-col gap-1 text-left'>
-                                    <strong className='text-sm'>Desvendando o Redux</strong>
+                                    <strong className='text-sm text-violet-600'>Desvendando o Redux</strong>
                                     <span className='text-xs text-zinc-400'>12 aulas</span>
                                 </div>                            
                                 
-                                <ChevronDown className='w-5 h-5 ml-auto text-zinc-400' />
+                                <ChevronDown className='w-5 h-5 ml-auto text-violet-600' />
                         </button>
 
                         <nav className='relative flex flex-col gap-4 p-6'>
@@ -70,11 +70,11 @@ export function Player() {
                                 </div>
 
                                 <div className='flex flex-col gap-1 text-left'>
-                                    <strong className='text-sm'>Desvendando o Redux</strong>
+                                    <strong className='text-sm text-violet-600'>Desvendando o Redux</strong>
                                     <span className='text-xs text-zinc-400'>12 aulas</span>
                                 </div>                            
                                 
-                                <ChevronDown className='w-5 h-5 ml-auto text-zinc-400' />
+                                <ChevronDown className='w-5 h-5 ml-auto text-violet-600' />
                         </button>
 
                         <nav className='relative flex flex-col gap-4 p-6'>
@@ -106,11 +106,11 @@ export function Player() {
                                 </div>
 
                                 <div className='flex flex-col gap-1 text-left'>
-                                    <strong className='text-sm'>Desvendando o Redux</strong>
+                                    <strong className='text-sm text-violet-600'>Desvendando o Redux</strong>
                                     <span className='text-xs text-zinc-400'>12 aulas</span>
                                 </div>                            
                                 
-                                <ChevronDown className='w-5 h-5 ml-auto text-zinc-400' />
+                                <ChevronDown className='w-5 h-5 ml-auto text-violet-600' />
                         </button>
 
                         <nav className='relative flex flex-col gap-4 p-6'>
