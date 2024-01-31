d3dc7c0fa0740612d08685299b754f218f89255e
lithproroot1399 <prorootinfo@gmail.com>
2024-01-31 13:24:51 +0000

    Utilizando huk

--- src/store/slices/player.ts
+++ src/store/slices/player.ts
@@ -1,4 +1,5 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { useAppSelector } from "..";
 
 const playerSlice = createSlice({
     name: 'player',
@@ -40,6 +41,20 @@ const playerSlice = createSlice({
       },
 
       next: (state) => {
+        const nextLessonIndex = state.currentLessonIndex + 1
+        const nextLesson = state.course.modules[state.currentModuleIndex].lessons[nextLessonIndex]
+
+        if (nextLesson) {
+          state.currentLessonIndex = nextLessonIndex
+        } else {
+          const nextModuleIndex = state.currentModuleIndex + 1
+          const nextModule = state.course.modules[nextModuleIndex]
+
+          if (nextModule) {
+            state.currentModuleIndex = nextModuleIndex
+            state.currentLessonIndex = 0
+          }
+        }
 
       },
     },
@@ -48,3 +63,14 @@ const playerSlice = createSlice({
 export const player = playerSlice.reducer
 
 export const { play, next } = playerSlice.actions
+
+export const useCurrentLesson = () => {
+  return useAppSelector(state => {        
+    const { currentModuleIndex, currentLessonIndex } = state.player
+    
+    const currentModule = state.player.course.modules[currentModuleIndex]
+    const currentLesson = currentModule.lessons[currentLessonIndex]
+
+    return {currentModule, currentLesson}
+})
+}
