66cd7a17e5fe15c63f78a198f88066b8351454ba
lithproroot1399 <prorootinfo@gmail.com>
2024-01-31 11:15:09 +0000

    Avançar video ao fim do atual

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,12 +1,20 @@
 import ReactPlayer from "react-player";
 import { useAppSelector } from "../store";
+import { useDispatch } from "react-redux";
+import { next } from "../store/slices/player";
 
 export function Video() {
+    const dispatch = useDispatch()
+
     const lesson = useAppSelector(state => {
         const { currentModuleIndex, currentLessonIndex } = state.player;
         const currentLesson = state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex];
         return currentLesson;
-    });
+    })
+
+    function handlePlayNext() {
+        dispatch(next())
+    }
 
     return (
         <div className="w-full bg-zinc-900 aspect-video">
@@ -14,6 +22,7 @@ export function Video() {
                 width="100%"
                 height="100%"
                 controls
+                onEnded={handlePlayNext}
                 url={`https://www.youtube.com/watch?v=${lesson.id}`} 
             />
         </div>
